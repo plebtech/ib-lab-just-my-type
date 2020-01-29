893f2c594702360603cffc7641008acb280527ad
Nick Mc <nbmc@protonmail.com>
2020-01-29 14:05:00 -0600

    funcionality to step through sentences and characters.

--- scripts.js
+++ scripts.js
@@ -2,14 +2,11 @@
 $(document).ready(function () {
     console.log("Loading..");
     $(keysUpper).toggle();
+    letterWatcher();
 });
 
 // save old key background color.
 let oldBg = $('#126').css("background-color");
-
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-let currentSentence = 0;
-$('#sentence').append(sentences[currentSentence]);
 
 // shortcut variables.
 let keysUpper = '#keyboard-upper-container';
@@ -47,23 +44,44 @@ $(document).keypress(function (event) {
 function keyCheck(event) {
     keyPressed = event.keyCode;
     keyValue = String.fromCharCode(event.which);
-    console.log(keyValue);
+    // console.log(keyValue);
 }
 function keyHilight() {
     $(`#${keyPressed}`).css("background-color", "#ffff00");
 }
-function keyClear() {
-    if ($(keyPressed).keydown() === false) {
-        console.log("no longer pressed");
-        $(`#${keyPressed}`).css("background-color", oldBg);
-        $(keyPressed).bind('keyup', function(event) {
-            $(`#${keyPressed}`).css("background-color", oldBg);
-        });
-    }
-}
-$(document).bind('keyup', function(event) {
+$(document).bind('keyup', function (event) {
     $('.key').css("background-color", oldBg);
 });
 // END hilighting.
+
+// BEGIN sentence/letter update.
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+let sentNow = 0;
+let charNow = 0;
+$(document).keypress(letterWatcher);
+function letterWatcher() {
+    if (charNow === sentences[sentNow].length) {
+        sentNow++;
+        charNow = 1;
+        updateSent();
+        updateChar();
+        // console.log("ding");
+        // console.log(sentences[sentNow]);
+        // console.log(charNow);
+    } else {
+        updateSent();
+        updateChar();
+        charNow++;
+        // console.log(charNow);
+    }
+}
+function updateSent() {
+    $('#sentence').text(sentences[sentNow]);
+}
+function updateChar() {
+    $('#target-letter').text(sentences[sentNow].charAt(charNow));
+}
+
+// END sentence/letter update.
 
 console.log("Loaded.");
