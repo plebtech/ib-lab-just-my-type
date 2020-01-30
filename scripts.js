4e900e0d166401329662a9986ae0872494155dc2
Nick Mc <nbmc@protonmail.com>
2020-01-30 11:33:24 -0600

    logic for finishing all sentences.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
-// listen for DOM content to load.
+// DOM ready loader.
 $(document).ready(function () {
     console.log("Loading..");
-    $(keysUpper).toggle();
+    $(KEYS_UPPER).toggle();
     letterWatcher();
 });
 
-// save old key background color.
-let oldBg = $('#126').css("background-color");
+// global variables.
+const OLD_BG = $('#126').css("background-color"); // save original key bg color.
+const SENTENCES = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+const KEYS_UPPER = '#keyboard-upper-container'; // shortcut.
+const KEYS_LOWER = '#keyboard-lower-container'; // shortcut.
+let shiftPressed = false; // tracks pressing of shift modifier.
+let keyPressed; // tracks current keypress.
+let keyValue;   // string of keyPressed.
+let sentNow = 0; // tracks which sentence the user is on.
+let charNow = 0; // tracks which character the user is on (within the current sentence).
 
-// shortcut variables.
-let keysUpper = '#keyboard-upper-container';
-let keysLower = '#keyboard-lower-container';
-
-// BEGIN shift listening.
-let shiftPressed = false;
+// shift listening.
 $(document).bind('keydown', function (event) {
     if (event.which === 16) {
         shiftPressed = true;
@@ -26,22 +29,23 @@ $(document).bind('keyup', function (event) {
 })
 $(document).bind('keydown keyup', function (event) {
     if (shiftPressed === true) {
-        $(keysUpper).show();
-        $(keysLower).hide();
+        $(KEYS_UPPER).show();
+        $(KEYS_LOWER).hide();
     } else {
-        $(keysUpper).hide();
-        $(keysLower).show();
+        $(KEYS_UPPER).hide();
+        $(KEYS_LOWER).show();
     }
 })
-// END shift listening.
 
-// BEGIN hilighting.
-let keyPressed;
-$(document).keypress(function (event) {
-    keyCheck(event);
-    keyHilight();
-});
-function keyCheck(event) {
+// keypress functions.
+$(document).keypress(keypressMaster);
+function keypressMaster() {
+    keySet(event);    // sets keyPressed variable.
+    keyHilight();       // hilights currently pressed key.
+    letterWatcher();    // updates current sentence/character.
+    // keyCheck();
+}
+function keySet(event) {
     keyPressed = event.keyCode;
     keyValue = String.fromCharCode(event.which);
     // console.log(keyValue);
@@ -49,39 +53,49 @@ function keyCheck(event) {
 function keyHilight() {
     $(`#${keyPressed}`).css("background-color", "#ffff00");
 }
+// function keyCheck() {
+//     console.log(keyValue);
+//     console.log(SENTENCES[sentNow].charCodeAt(charNow));
+//     if (keyPressed === charNow) {
+//         $('#feedback').html('<span class="glyphicon glyphicon-ok"></span>');
+//         console.log("ok");
+//     } else {
+//         $('#feedback').html('<span class="glyphicon glyphicon-remove"></span>');
+//         console.log("bad");
+//     }
+// }
 $(document).bind('keyup', function (event) {
-    $('.key').css("background-color", oldBg);
+    $('.key').css("background-color", OLD_BG);
 });
-// END hilighting.
-
-// BEGIN sentence/letter update.
-let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-let sentNow = 0;
-let charNow = 0;
-$(document).keypress(letterWatcher);
 function letterWatcher() {
-    if (charNow === sentences[sentNow].length) {
+    if (charNow === SENTENCES[sentNow].length) {
         sentNow++;
-        charNow = 1;
-        updateSent();
-        updateChar();
-        // console.log("ding");
-        // console.log(sentences[sentNow]);
-        // console.log(charNow);
+        console.log(sentNow + " " + SENTENCES.length);
+        if (sentNow >= SENTENCES.length) {
+            $('#sentence').text("You've reached the end!");
+            $('#target-letter').text('');
+            $(document).unbind('keypress', keypressMaster);
+            $(document).keypress(function (event) {
+                keySet(event);
+                keyHilight();
+            });
+        } else {
+            charNow = 0;
+            updateSent();
+            updateChar();
+            charNow++;
+        }
     } else {
         updateSent();
         updateChar();
         charNow++;
-        // console.log(charNow);
     }
 }
 function updateSent() {
-    $('#sentence').text(sentences[sentNow]);
+    $('#sentence').text(SENTENCES[sentNow]);
 }
 function updateChar() {
-    $('#target-letter').text(sentences[sentNow].charAt(charNow));
+    $('#target-letter').text(SENTENCES[sentNow].charAt(charNow));
 }
-
-// END sentence/letter update.
 
 console.log("Loaded.");
